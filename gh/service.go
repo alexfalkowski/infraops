02ace7ae6b0234c19fe77f2ff3e67fdf8906feca
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-23 14:36:24 +0100

    refactor(gh): move to services (#28)

---
+++ gh/service.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func createTemplate(ctx *pulumi.Context) error {
+	return createService(ctx, "go-service-template", "A template for go services.")
+}
