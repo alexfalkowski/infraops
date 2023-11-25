4af75899139f683c7c813ad131c05379a3871cbf
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-25 07:30:30 +0100

    feat(gh): consistently create the respositories (#31)

--- gh/service_test.go
+++ gh/service_test.go
@@ -4,71 +4,71 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTemplate(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
 		err := createTemplate(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		return nil
 	}, pulumi.WithMocks("project", "stack", mocks(0)))
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func TestStatus(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
 		err := createStatus(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		return nil
 	}, pulumi.WithMocks("project", "stack", mocks(0)))
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func TestStandort(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
 		err := createStandort(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		return nil
 	}, pulumi.WithMocks("project", "stack", mocks(0)))
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func TestAuth(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
 		err := createAuth(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		return nil
 	}, pulumi.WithMocks("project", "stack", mocks(0)))
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func TestKonfig(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
 		err := createKonfig(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		return nil
 	}, pulumi.WithMocks("project", "stack", mocks(0)))
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
 
 func TestMigrieren(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
 		err := createMigrieren(ctx)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		return nil
 	}, pulumi.WithMocks("project", "stack", mocks(0)))
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
