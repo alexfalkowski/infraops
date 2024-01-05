75c04d9471f950f87b743311ca9891104b0d6d39
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-01-05 12:43:34 +0100

    fix(gh): use generated url (#53)

--- area/gh/main.go
+++ area/gh/main.go
@@ -32,77 +32,77 @@ func createDocker(ctx *pulumi.Context) error {
 }
 
 func createAppConfig(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/app-config"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/app-config"}
 	_, err := gh.CreateMasterRepository(ctx, "app-config", "A place for all of my application configuration.", args)
 
 	return err
 }
 
 func createBin(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/bin"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/bin"}
 	_, err := gh.CreateMasterRepository(ctx, "bin", "A place for common executables.", args)
 
 	return err
 }
 
 func createNonnative(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/nonnative"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/nonnative"}
 	_, err := gh.CreateMasterRepository(ctx, "nonnative", "Allows you to keep using the power of ruby to test other systems.", args)
 
 	return err
 }
 
 func createGoHealth(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/go-health"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/go-health"}
 	_, err := gh.CreateMasterRepository(ctx, "go-health", "Health monitoring pattern in Go.", args)
 
 	return err
 }
 
 func createGoService(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/go-service"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/go-service"}
 	_, err := gh.CreateMasterRepository(ctx, "go-service", "A framework to build services in go.", args)
 
 	return err
 }
 
 func createGoServiceTemplate(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/go-service-template", IsTemplate: true}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/go-service-template", IsTemplate: true}
 	_, err := gh.CreateMasterRepository(ctx, "go-service-template", "A template for go services.", args)
 
 	return err
 }
 
 func createStatus(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/status"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/status"}
 	_, err := gh.CreateMasterRepository(ctx, "status", "An alternative to https://httpstat.us/", args)
 
 	return err
 }
 
 func createStandort(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/standort"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/standort"}
 	_, err := gh.CreateMasterRepository(ctx, "standort", "Standort provides location based information.", args)
 
 	return err
 }
 
 func createAuth(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/auth"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/auth"}
 	_, err := gh.CreateMasterRepository(ctx, "auth", "Auth provides all your authn and authz needs.", args)
 
 	return err
 }
 
 func createKonfig(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/konfig"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/konfig"}
 	_, err := gh.CreateMasterRepository(ctx, "konfig", "Konfig is a configuration system for application configuration.", args)
 
 	return err
 }
 
 func createMigrieren(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://github.com/alexfalkowski/migrieren"}
+	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/migrieren"}
 	_, err := gh.CreateMasterRepository(ctx, "migrieren", "Migrieren provides a way to migrate your databases.", args)
 
 	return err
