ba56d0a655f196607b90d4b2d6ba112aa7615e80
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-01-05 14:03:00 +0100

    feat(gh): add branch protection (#55)

--- gh/gh.go
+++ gh/gh.go
@@ -10,6 +10,7 @@ type RepositoryArgs struct {
 	HomepageURL string
 	IsTemplate  bool
 	Topics      []string
+	Checks      []string
 }
 
 // CreateMasterRepository for gh.
@@ -23,6 +24,19 @@ func CreateMainRepository(ctx *pulumi.Context, name, description string, args *R
 }
 
 func createRepository(ctx *pulumi.Context, name, description, branch string, args *RepositoryArgs) (*github.Repository, error) {
+	r, err := repository(ctx, name, description, branch, args)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := branchProtection(ctx, name, branch, r.NodeId, args); err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func repository(ctx *pulumi.Context, name, description, branch string, args *RepositoryArgs) (*github.Repository, error) {
 	return github.NewRepository(ctx, name, &github.RepositoryArgs{
 		AllowMergeCommit:    pulumi.Bool(false),
 		AllowRebaseMerge:    pulumi.Bool(false),
@@ -55,4 +69,26 @@ func createRepository(ctx *pulumi.Context, name, description, branch string, arg
 		Visibility:             pulumi.String("public"),
 		VulnerabilityAlerts:    pulumi.Bool(true),
 	})
+}
+
+func branchProtection(ctx *pulumi.Context, name, branch string, id pulumi.StringInput, args *RepositoryArgs) error {
+	_, err := github.NewBranchProtection(ctx, name, &github.BranchProtectionArgs{
+		Pattern:               pulumi.String(branch),
+		RepositoryId:          id,
+		RequiredLinearHistory: pulumi.Bool(true),
+		RequiredPullRequestReviews: github.BranchProtectionRequiredPullRequestReviewArray{
+			&github.BranchProtectionRequiredPullRequestReviewArgs{
+				DismissStaleReviews:          pulumi.Bool(true),
+				RequiredApprovingReviewCount: pulumi.Int(0),
+			},
+		},
+		RequiredStatusChecks: github.BranchProtectionRequiredStatusCheckArray{
+			&github.BranchProtectionRequiredStatusCheckArgs{
+				Contexts: pulumi.ToStringArray(args.Checks),
+				Strict:   pulumi.Bool(true),
+			},
+		},
+	})
+
+	return err
 }
