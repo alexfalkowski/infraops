e79f7a8c3756ea460c7f10112d04fb022f058d3c
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-01-07 17:23:14 +0100

    feat(gh): move to repo model (#61)

--- area/gh/main.go
+++ area/gh/main.go
@@ -18,105 +18,147 @@ var fns = []createFn{
 }
 
 func createInfraOps(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/infraops", Checks: []string{"ci/circleci: build"}}
-	_, err := gh.CreateRepository(ctx, "infraops", "A place where all infrastructure is taken care of.", args)
+	repo := &gh.Repository{
+		Name: "infraops", Description: "A place where all infrastructure is taken care of.",
+		HomepageURL: "https://alexfalkowski.github.io/infraops", Checks: []string{"ci/circleci: build"},
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createPages(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io"}
-	_, err := gh.CreateRepository(ctx, "alexfalkowski.github.io", "A site for my profile.", args)
+	repo := &gh.Repository{
+		Name: "alexfalkowski.github.io", Description: "A site for my profile.",
+		HomepageURL: "https://alexfalkowski.github.io",
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createDocker(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{Topics: []string{"docker", "ruby", "golang"}, Checks: []string{"ci/circleci: lint", "ci/circleci: build"}}
-	_, err := gh.CreateRepository(ctx, "docker", "Common setup used for my projects.", args)
+	repo := &gh.Repository{
+		Name: "docker", Description: "Common setup used for my projects.",
+		Topics: []string{"docker", "ruby", "golang"}, Checks: []string{"ci/circleci: lint", "ci/circleci: build"},
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createAppConfig(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/app-config"}
-	_, err := gh.CreateRepository(ctx, "app-config", "A place for all of my application configuration.", args)
+	repo := &gh.Repository{
+		Name: "app-config", Description: "A place for all of my application configuration.",
+		HomepageURL: "https://alexfalkowski.github.io/app-config",
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createBin(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/bin", Checks: []string{"ci/circleci: build"}}
-	_, err := gh.CreateRepository(ctx, "bin", "A place for common executables.", args)
+	repo := &gh.Repository{
+		Name: "bin", Description: "A place for common executables.",
+		HomepageURL: "https://alexfalkowski.github.io/bin", Checks: []string{"ci/circleci: build"},
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createNonnative(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/nonnative", Checks: []string{"ci/circleci: build"}}
-	_, err := gh.CreateRepository(ctx, "nonnative", "Allows you to keep using the power of ruby to test other systems.", args)
+	repo := &gh.Repository{
+		Name: "nonnative", Description: "Allows you to keep using the power of ruby to test other systems.",
+		HomepageURL: "https://alexfalkowski.github.io/nonnative", Checks: []string{"ci/circleci: build"},
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createGoHealth(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/go-health", Checks: []string{"ci/circleci: build"}}
-	_, err := gh.CreateRepository(ctx, "go-health", "Health monitoring pattern in Go.", args)
+	repo := &gh.Repository{
+		Name: "go-health", Description: "Health monitoring pattern in Go.",
+		HomepageURL: "https://alexfalkowski.github.io/go-health", Checks: []string{"ci/circleci: build"},
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createGoService(ctx *pulumi.Context) error {
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/go-service", Checks: []string{"ci/circleci: build"}}
-	_, err := gh.CreateRepository(ctx, "go-service", "A framework to build services in go.", args)
+	repo := &gh.Repository{
+		Name: "go-service", Description: "A framework to build services in go.",
+		HomepageURL: "https://alexfalkowski.github.io/go-service", Checks: []string{"ci/circleci: build"},
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createGoServiceTemplate(ctx *pulumi.Context) error {
 	checks := []string{"ci/circleci: build-service", "ci/circleci: build-docker"}
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/go-service-template", Checks: checks, IsTemplate: true}
-	_, err := gh.CreateRepository(ctx, "go-service-template", "A template for go services.", args)
+	repo := &gh.Repository{
+		Name: "go-service-template", Description: "A template for go services.",
+		HomepageURL: "https://alexfalkowski.github.io/go-service-template", Checks: checks, IsTemplate: true,
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createStatus(ctx *pulumi.Context) error {
 	checks := []string{"ci/circleci: build-service", "ci/circleci: build-docker"}
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/status", Checks: checks}
-	_, err := gh.CreateRepository(ctx, "status", "An alternative to https://httpstat.us/", args)
+	repo := &gh.Repository{
+		Name: "status", Description: "An alternative to https://httpstat.us/",
+		HomepageURL: "https://alexfalkowski.github.io/status", Checks: checks,
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createStandort(ctx *pulumi.Context) error {
 	checks := []string{"ci/circleci: build-service", "ci/circleci: build-docker", "ci/circleci: features-grpc", "ci/circleci: features-http", "ci/circleci: features-coveralls"}
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/standort", Checks: checks}
-	_, err := gh.CreateRepository(ctx, "standort", "Standort provides location based information.", args)
+	repo := &gh.Repository{
+		Name: "standort", Description: "Standort provides location based information.",
+		HomepageURL: "https://alexfalkowski.github.io/standort", Checks: checks,
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createAuth(ctx *pulumi.Context) error {
 	checks := []string{"ci/circleci: build-service", "ci/circleci: build-docker"}
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/auth", Checks: checks}
-	_, err := gh.CreateRepository(ctx, "auth", "Auth provides all your authn and authz needs.", args)
+	repo := &gh.Repository{
+		Name: "auth", Description: "Auth provides all your authn and authz needs.",
+		HomepageURL: "https://alexfalkowski.github.io/auth", Checks: checks,
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createKonfig(ctx *pulumi.Context) error {
 	checks := []string{"ci/circleci: build-service", "ci/circleci: build-docker", "ci/circleci: features-grpc", "ci/circleci: features-http", "ci/circleci: features-coveralls"}
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/konfig", Checks: checks}
-	_, err := gh.CreateRepository(ctx, "konfig", "Konfig is a configuration system for application configuration.", args)
+	repo := &gh.Repository{
+		Name: "konfig", Description: "Konfig is a configuration system for application configuration.",
+		HomepageURL: "https://alexfalkowski.github.io/konfig", Checks: checks,
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
 
 func createMigrieren(ctx *pulumi.Context) error {
 	checks := []string{"ci/circleci: build-service", "ci/circleci: build-docker"}
-	args := &gh.RepositoryArgs{HomepageURL: "https://alexfalkowski.github.io/migrieren", Checks: checks}
-	_, err := gh.CreateRepository(ctx, "migrieren", "Migrieren provides a way to migrate your databases.", args)
+	repo := &gh.Repository{
+		Name: "migrieren", Description: "Migrieren provides a way to migrate your databases.",
+		HomepageURL: "https://alexfalkowski.github.io/migrieren", Checks: checks,
+	}
+	_, err := gh.CreateRepository(ctx, repo)
 
 	return err
 }
