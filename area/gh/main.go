76f5ef96c237f04a7b71a6b7db77a6e882bc8867
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-04-18 18:05:17 +0200

    feat(gh): add test repository (#116)

--- area/gh/main.go
+++ area/gh/main.go
@@ -8,6 +8,7 @@ import (
 type createFn func(ctx *pulumi.Context) error
 
 var fns = []createFn{
+	createTest,
 	createPages, createInfraOps,
 	createDocker, createAppConfig,
 	createBin, createNonnative,
@@ -15,6 +16,17 @@ var fns = []createFn{
 	createGoServiceTemplate, createStatus,
 	createStandort, createAuth,
 	createKonfig, createMigrieren,
+}
+
+func createTest(ctx *pulumi.Context) error {
+	checks := []string{"ci/circleci: build-service", "ci/circleci: build-docker"}
+	repo := &gh.Repository{
+		Name: "test", Description: "Test repository.",
+		HomepageURL: "https://alexfalkowski.github.io/test", Checks: checks,
+		Template: gh.Template{Owner: "alexfalkowski", Repository: "go-service-template"},
+	}
+
+	return gh.CreateRepository(ctx, repo)
 }
 
 func createInfraOps(ctx *pulumi.Context) error {
