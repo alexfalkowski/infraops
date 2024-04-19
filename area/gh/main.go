84a42be14055be3f4b0b77bbda20fcc6733274e1
Alex Falkowski <alexrfalkowski@gmail.com>
2024-04-19 08:04:47 +0200

    feat(gh): remove super service

--- area/gh/main.go
+++ area/gh/main.go
@@ -8,7 +8,6 @@ import (
 type createFn func(ctx *pulumi.Context) error
 
 var fns = []createFn{
-	createSuperService,
 	createSite, createInfraOps,
 	createDocker, createAppConfig,
 	createBin, createNonnative,
@@ -16,17 +15,6 @@ var fns = []createFn{
 	createGoServiceTemplate, createStatus,
 	createStandort, createAuth,
 	createKonfig, createMigrieren,
-}
-
-func createSuperService(ctx *pulumi.Context) error {
-	repo := &gh.Repository{
-		Name: "superservice", Description: "A super service.",
-		HomepageURL: "https://alexfalkowski.github.io/superservice",
-		Template:    gh.Template{Owner: "alexfalkowski", Repository: "go-service-template"},
-		EnablePages: true,
-	}
-
-	return gh.CreateRepository(ctx, repo)
 }
 
 func createInfraOps(ctx *pulumi.Context) error {
