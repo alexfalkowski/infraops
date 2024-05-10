0d94aa8cdd173cd380daacaa2335509b22bba916
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-10 16:45:06 +0200

    feat(gh): add servicectl

---
+++ area/gh/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/alexfalkowski/infraops/gh"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func createGoClientTemplate(ctx *pulumi.Context) error {
+	checks := []string{"ci/circleci: build-client", "ci/circleci: build-docker"}
+	repo := &gh.Repository{
+		Name: "go-client-template", Description: "A template for go clients.",
+		HomepageURL: "https://alexfalkowski.github.io/go-client-template", Checks: checks,
+		IsTemplate: true, Template: gh.Template{Owner: "alexfalkowski", Repository: "go-service-template"},
+		EnablePages: true,
+	}
+
+	return gh.CreateRepository(ctx, repo)
+}
+
+func createServiceControl(ctx *pulumi.Context) error {
+	checks := []string{"ci/circleci: build-client", "ci/circleci: build-docker"}
+	repo := &gh.Repository{
+		Name: "servicectl", Description: "A tool for go-service and go-service-templates.",
+		HomepageURL: "https://alexfalkowski.github.io/servicectl", Checks: checks,
+		IsTemplate: false, Template: gh.Template{Owner: "alexfalkowski", Repository: "go-client-template"},
+		EnablePages: false,
+	}
+
+	return gh.CreateRepository(ctx, repo)
+}
