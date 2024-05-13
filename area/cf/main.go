598559bdc4c8b14176777b4bc1afcaecb56c6358
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-13 21:29:53 +0200

    fix(cf): change account name

--- area/cf/main.go
+++ area/cf/main.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-cloudflare/sdk/v5/go/cloudflare"
+	"github.com/alexfalkowski/infraops/cf"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		a, err := cloudflare.NewAccount(ctx, "main", &cloudflare.AccountArgs{
-			Name: pulumi.String("Alexrfalkowski@gmail.com's Account"),
-		})
-		if err != nil {
-			return err
-		}
+		err := cf.CreateZone(ctx, "lean-thoughts")
 
-		_, err = cloudflare.NewZone(ctx, "lean-thoughts", &cloudflare.ZoneArgs{
-			AccountId: a.ID(),
-			Plan:      pulumi.String("free"),
-			Zone:      pulumi.String("lean-thoughts.com"),
-		})
-		if err != nil {
-			return err
-		}
-
-		return nil
+		return err
 	})
 }
