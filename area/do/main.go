a0fb48668ce6fa2816600adc5d2f1261e204bc64
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-14 08:56:27 +0200

    feat(do): add lean thoughts project

--- area/do/main.go
+++ area/do/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"github.com/alexfalkowski/infraops/do"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(_ *pulumi.Context) error {
-		return nil
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		lt := &do.Project{
+			Name:        "lean-thoughts",
+			Description: "The lean thoughts domain",
+		}
+		err := do.CreateProject(ctx, lt)
+
+		return err
 	})
 }
