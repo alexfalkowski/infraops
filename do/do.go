2c54befbbcb6aa731cbaf29748abd6fe7f0ef5de
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-14 14:25:52 +0200

    feat(do): add kubernetes cluster (#166)

--- do/do.go
+++ do/do.go
@@ -13,7 +13,12 @@ type Project struct {
 
 // CreateProject for do.
 func CreateProject(ctx *pulumi.Context, project *Project) error {
-	_, err := createVPC(ctx, project)
+	v, err := createVPC(ctx, project)
+	if err != nil {
+		return err
+	}
+
+	c, err := createCluster(ctx, v, project)
 	if err != nil {
 		return err
 	}
@@ -24,6 +29,9 @@ func CreateProject(ctx *pulumi.Context, project *Project) error {
 		Environment: pulumi.String("Production"),
 		Purpose:     pulumi.String("Service or API"),
 		IsDefault:   pulumi.Bool(false),
+		Resources: pulumi.StringArray{
+			c.ClusterUrn,
+		},
 	}
 	_, err = digitalocean.NewProject(ctx, project.Name, args)
 
@@ -33,9 +41,33 @@ func CreateProject(ctx *pulumi.Context, project *Project) error {
 func createVPC(ctx *pulumi.Context, p *Project) (*digitalocean.Vpc, error) {
 	args := &digitalocean.VpcArgs{
 		Name:        pulumi.String(p.Name),
-		Region:      pulumi.String("fra1"),
+		Region:      digitalocean.RegionFRA1,
 		Description: pulumi.String(p.Description),
 	}
 
 	return digitalocean.NewVpc(ctx, p.Name, args)
+}
+
+func createCluster(ctx *pulumi.Context, v *digitalocean.Vpc, p *Project) (*digitalocean.KubernetesCluster, error) {
+	args := &digitalocean.KubernetesClusterArgs{
+		NodePool: &digitalocean.KubernetesClusterNodePoolArgs{
+			Name:      pulumi.String(p.Name),
+			Size:      digitalocean.DropletSlugDropletS1VCPU2GB,
+			AutoScale: pulumi.Bool(false),
+			MaxNodes:  pulumi.Int(1),
+			MinNodes:  pulumi.Int(1),
+			NodeCount: pulumi.Int(1),
+		},
+		Region:                        digitalocean.RegionFRA1,
+		Version:                       pulumi.String("1.29.1-do.0"),
+		AutoUpgrade:                   pulumi.Bool(false),
+		DestroyAllAssociatedResources: pulumi.Bool(true),
+		Ha:                            pulumi.Bool(false),
+		Name:                          pulumi.String(p.Name),
+		RegistryIntegration:           pulumi.Bool(false),
+		SurgeUpgrade:                  pulumi.Bool(false),
+		VpcUuid:                       v.VpcUrn,
+	}
+
+	return digitalocean.NewKubernetesCluster(ctx, p.Name, args)
 }
