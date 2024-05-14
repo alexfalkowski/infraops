c4688dad1b94465f3256f0555fc5eea0ba5ce4bf
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-14 22:37:04 +0200

    feat(k8s): remove demo (#176)

--- area/k8s/main.go
+++ area/k8s/main.go
@@ -1,65 +1,11 @@
 package main
 
 import (
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		appName := "nginx"
-		appLabels := pulumi.StringMap{
-			"app": pulumi.String(appName),
-		}
-
-		_, err := appsv1.NewDeployment(ctx, appName, &appsv1.DeploymentArgs{
-			Metadata: metav1.ObjectMetaArgs{
-				Name: pulumi.String(appName),
-			},
-			Spec: appsv1.DeploymentSpecArgs{
-				Selector: &metav1.LabelSelectorArgs{
-					MatchLabels: appLabels,
-				},
-				Replicas: pulumi.Int(1),
-				Template: &corev1.PodTemplateSpecArgs{
-					Metadata: &metav1.ObjectMetaArgs{
-						Labels: appLabels,
-					},
-					Spec: &corev1.PodSpecArgs{
-						Containers: corev1.ContainerArray{
-							corev1.ContainerArgs{
-								Name:  pulumi.String("nginx"),
-								Image: pulumi.String("nginx"),
-							},
-						},
-					},
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
-
-		_, err = corev1.NewService(ctx, appName, &corev1.ServiceArgs{
-			Metadata: &metav1.ObjectMetaArgs{
-				Name:   pulumi.String(appName),
-				Labels: appLabels,
-			},
-			Spec: &corev1.ServiceSpecArgs{
-				Type: pulumi.String("LoadBalancer"),
-				Ports: &corev1.ServicePortArray{
-					&corev1.ServicePortArgs{
-						Port:       pulumi.Int(80),
-						TargetPort: pulumi.Int(80),
-						Protocol:   pulumi.String("TCP"),
-					},
-				},
-				Selector: appLabels,
-			},
-		})
-
-		return err
+	pulumi.Run(func(_ *pulumi.Context) error {
+		return nil
 	})
 }
