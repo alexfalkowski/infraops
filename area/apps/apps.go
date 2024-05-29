36e09667c51da2f9f4ba7a01b96d417095ec2a1d
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-29 20:34:30 +0200

    feat(apps): update versions (#285)

--- area/apps/apps.go
+++ area/apps/apps.go
@@ -9,7 +9,7 @@ func createKonfig(ctx *pulumi.Context) error {
 	a := &app.App{
 		ID:            "1115c470-ccc9-4daf-8459-ef1e19c40afe",
 		Name:          "konfig",
-		Version:       "1.135.0",
+		Version:       "1.135.1",
 		Memory:        app.Memory{Min: "128Mi", Max: "256Mi"},
 		SecretVolumes: []string{"gh"},
 	}
@@ -21,8 +21,8 @@ func createStandort(ctx *pulumi.Context) error {
 	a := &app.App{
 		ID:            "28c679dc-5924-47e8-ac48-73cd842ba5cd",
 		Name:          "standort",
-		InitVersion:   "1.135.0",
-		Version:       "2.96.0",
+		InitVersion:   "1.135.1",
+		Version:       "2.96.1",
 		ConfigVersion: "1.8.0",
 		Memory:        app.Memory{Min: "64Mi", Max: "128Mi"},
 	}
@@ -34,8 +34,8 @@ func createBezeichner(ctx *pulumi.Context) error {
 	a := &app.App{
 		ID:            "98968ca0-4ada-4856-8547-210f92b838ea",
 		Name:          "bezeichner",
-		InitVersion:   "1.135.0",
-		Version:       "1.98.0",
+		InitVersion:   "1.135.1",
+		Version:       "1.98.1",
 		ConfigVersion: "1.7.0",
 		Memory:        app.Memory{Min: "64Mi", Max: "128Mi"},
 	}
