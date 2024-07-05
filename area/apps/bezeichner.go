438a340693476933ab4495c040dee398c483ff9c
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-05 07:59:04 +0200

    feat(apps): update versions

--- area/apps/bezeichner.go
+++ area/apps/bezeichner.go
@@ -10,8 +10,8 @@ func createBezeichner(ctx *pulumi.Context) error {
 		ID:            "98968ca0-4ada-4856-8547-210f92b838ea",
 		Name:          "bezeichner",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "1.189.0",
-		Version:       "1.131.0",
+		InitVersion:   "1.190.0",
+		Version:       "1.132.0",
 		ConfigVersion: "1.9.0",
 		Memory:        app.Memory{Min: "64Mi", Max: "128Mi"},
 	}
