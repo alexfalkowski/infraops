15ac589dfd8e865a1c4a0695df855cd0de36ea18
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-07-23 15:22:46 +0200

    feat(apps): update web to v0.20.0 (#391)

--- area/apps/web.go
+++ area/apps/web.go
@@ -11,7 +11,7 @@ func createWeb(ctx *pulumi.Context) error {
 		Name:          "web",
 		Domain:        "lean-thoughts.com",
 		InitVersion:   "1.203.0",
-		Version:       "0.19.0",
+		Version:       "0.20.0",
 		ConfigVersion: "1.0.0",
 		Memory:        app.Memory{Min: "64Mi", Max: "128Mi"},
 	}
