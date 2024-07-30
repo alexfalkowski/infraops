4ed662f7e771a9fd878b86a306ef10a0537d9a87
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-30 08:07:36 +0200

    feat(apps): update versions

--- area/apps/konfig.go
+++ area/apps/konfig.go
@@ -10,7 +10,7 @@ func createKonfig(ctx *pulumi.Context) error {
 		ID:            "1115c470-ccc9-4daf-8459-ef1e19c40afe",
 		Name:          "konfig",
 		Domain:        "lean-thoughts.com",
-		Version:       "1.211.0",
+		Version:       "1.213.0",
 		Memory:        app.Memory{Min: "128Mi", Max: "256Mi"},
 		SecretVolumes: []string{"gh"},
 	}
