34603d10d1d80013e97380407da8a8a3523a119b
Alex Falkowski <alexrfalkowski@gmail.com>
2024-08-05 12:39:36 +0200

    feat(apps): use new init container

--- area/apps/lean/bezeichner.go
+++ area/apps/lean/bezeichner.go
@@ -11,7 +11,7 @@ func createBezeichner(ctx *pulumi.Context) error {
 		Name:          "bezeichner",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "1.222.0",
+		InitVersion:   "0.2.0",
 		Version:       "1.159.0",
 		ConfigVersion: "1.10.1",
 		Resources: &app.Resources{
