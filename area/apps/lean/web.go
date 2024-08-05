869627ec8f932ebb1e2bcbe4a752feb25305bdb5
Alex Falkowski <alexrfalkowski@gmail.com>
2024-08-05 13:20:59 +0200

    feat(apps): update init to v0.3.0

--- area/apps/lean/web.go
+++ area/apps/lean/web.go
@@ -11,7 +11,7 @@ func createWeb(ctx *pulumi.Context) error {
 		Name:          "web",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.2.0",
+		InitVersion:   "0.3.0",
 		Version:       "0.37.0",
 		ConfigVersion: "1.1.0",
 		Resources: &app.Resources{
