d5866a1f9d73355af2617c628666917c3671517f
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-08-09 12:20:18 +0200

    feat(apps): update konfig (#438)

--- area/apps/lean/konfig.go
+++ area/apps/lean/konfig.go
@@ -11,13 +11,13 @@ func createKonfig(ctx *pulumi.Context) error {
 		Name:      "konfig",
 		Namespace: "lean",
 		Domain:    "lean-thoughts.com",
-		Version:   "1.227.0",
+		Version:   "1.229.0",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "250m", Max: "500m"},
 			Memory:  &app.Range{Min: "128Mi", Max: "256Mi"},
 			Storage: &app.Range{Min: "1Gi", Max: "2Gi"},
 		},
-		SecretVolumes: []string{"gh"},
+		SecretVolumes: []string{"gh", "konfig"},
 	}
 
 	return app.CreateApp(ctx, a)
