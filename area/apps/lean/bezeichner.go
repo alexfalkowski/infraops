626a10a755b275240d71535daf6159e53819abbb
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-08-14 21:49:52 +0200

    feat(apps): update versions (#450)

--- area/apps/lean/bezeichner.go
+++ area/apps/lean/bezeichner.go
@@ -11,8 +11,8 @@ func createBezeichner(ctx *pulumi.Context) error {
 		Name:          "bezeichner",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.10.0",
-		Version:       "1.166.0",
+		InitVersion:   "0.11.0",
+		Version:       "1.167.0",
 		ConfigVersion: "1.11.1",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
