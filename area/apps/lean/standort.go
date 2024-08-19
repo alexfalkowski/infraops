eb59d24c8e52707ba9ba61f7017bbf91ef870421
Alex Falkowski <alexrfalkowski@gmail.com>
2024-08-19 13:53:58 +0200

    feat(apps): update versions

--- area/apps/lean/standort.go
+++ area/apps/lean/standort.go
@@ -12,7 +12,7 @@ func createStandort(ctx *pulumi.Context) error {
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
 		InitVersion:   "0.13.0",
-		Version:       "2.162.0",
+		Version:       "2.164.0",
 		ConfigVersion: "1.12.1",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
--- area/apps/lean/web.go
+++ area/apps/lean/web.go
@@ -12,7 +12,7 @@ func createWeb(ctx *pulumi.Context) error {
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
 		InitVersion:   "0.13.0",
-		Version:       "0.43.0",
+		Version:       "0.44.0",
 		ConfigVersion: "1.2.1",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
