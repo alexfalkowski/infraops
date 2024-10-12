4296d43164be217a63ac54d5628b5846d18ba5bb
Alex Falkowski <alexrfalkowski@gmail.com>
2024-10-12 09:29:56 +0200

    feat(apps): update versions
    Use distroless static

--- area/apps/lean/standort.go
+++ area/apps/lean/standort.go
@@ -11,8 +11,8 @@ func createStandort(ctx *pulumi.Context) error {
 		Name:          "standort",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.39.0",
-		Version:       "2.189.0",
+		InitVersion:   "0.40.0",
+		Version:       "2.190.0",
 		ConfigVersion: "1.12.1",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
--- area/apps/lean/web.go
+++ area/apps/lean/web.go
@@ -11,8 +11,8 @@ func createWeb(ctx *pulumi.Context) error {
 		Name:          "web",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.39.0",
-		Version:       "0.70.0",
+		InitVersion:   "0.40.0",
+		Version:       "0.71.0",
 		ConfigVersion: "1.2.1",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
