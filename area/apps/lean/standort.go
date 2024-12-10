7453886d028bac2f5884da5ef0454eb603da03f3
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-12-10 17:29:52 +0100

    feat(apps): add kind to token (#578)
    https://github.com/alexfalkowski/go-service/pull/1058

--- area/apps/lean/standort.go
+++ area/apps/lean/standort.go
@@ -11,7 +11,7 @@ func createStandort(ctx *pulumi.Context) error {
 		Name:          "standort",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.57.0",
+		InitVersion:   "0.61.0",
 		Version:       "2.207.0",
 		ConfigVersion: "1.12.1",
 		Resources: &app.Resources{
