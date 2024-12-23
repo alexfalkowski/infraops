77a68fb8750ad0680fcc674935c500d2fcd36436
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-12-23 12:31:08 +0100

    feat(apps): update versions (#620)
    https://github.com/alexfalkowski/go-service/pull/1101

--- area/apps/lean/web.go
+++ area/apps/lean/web.go
@@ -11,8 +11,8 @@ func createWeb(ctx *pulumi.Context) error {
 		Name:          "web",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.80.0",
-		Version:       "0.109.0",
+		InitVersion:   "0.81.0",
+		Version:       "0.110.0",
 		ConfigVersion: "1.2.1",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
