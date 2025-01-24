229565c3cbea529b4fe873a0b220f95a6949bdf4
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-01-24 11:03:42 +0100

    feat(apps): update standort to v2.265.0 (#713)
    https://github.com/alexfalkowski/standort/pull/1092

--- area/apps/lean/standort.go
+++ area/apps/lean/standort.go
@@ -12,7 +12,7 @@ func createStandort(ctx *pulumi.Context) error {
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
 		InitVersion:   "0.113.0",
-		Version:       "2.264.0",
+		Version:       "2.265.0",
 		ConfigVersion: "1.13.0",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
