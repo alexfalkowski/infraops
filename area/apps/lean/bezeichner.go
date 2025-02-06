1591022672940e7399fab295af289694b6e77871
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-06 07:48:39 +0100

    feat(apps): update protobuf (#749)

--- area/apps/lean/bezeichner.go
+++ area/apps/lean/bezeichner.go
@@ -12,7 +12,7 @@ func createBezeichner(ctx *pulumi.Context) error {
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
 		InitVersion:   "0.137.0",
-		Version:       "1.294.0",
+		Version:       "1.295.0",
 		ConfigVersion: "1.12.0",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
