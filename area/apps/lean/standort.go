4e1a31af311344be360f52b06345afb7eddb71c7
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-09 10:55:28 +0100

    feat(apps): update versions (#760)
    https://github.com/alexfalkowski/go-service/pull/1278

--- area/apps/lean/standort.go
+++ area/apps/lean/standort.go
@@ -11,8 +11,8 @@ func createStandort(ctx *pulumi.Context) error {
 		Name:          "standort",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.144.0",
-		Version:       "2.295.0",
+		InitVersion:   "0.145.0",
+		Version:       "2.296.0",
 		ConfigVersion: "1.13.0",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
