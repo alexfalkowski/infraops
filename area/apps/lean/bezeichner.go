de1c67c2b364c82eac1db5eadfc782444c66e813
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-18 14:44:56 +0100

    feat(apps): update konfigctl to v0.175.0 (#802)
    https://github.com/alexfalkowski/konfigctl/pull/307

--- area/apps/lean/bezeichner.go
+++ area/apps/lean/bezeichner.go
@@ -11,7 +11,7 @@ func createBezeichner(ctx *pulumi.Context) error {
 		Name:          "bezeichner",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.174.0",
+		InitVersion:   "0.175.0",
 		Version:       "1.330.0",
 		ConfigVersion: "1.12.0",
 		Resources: &app.Resources{
