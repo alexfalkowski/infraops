24e5d8fd61668c5a9c25947dec9ea2f3c4a916c3
Alex Falkowski <alexrfalkowski@gmail.com>
2025-02-18 14:41:45 +0100

    feat(apps): update konfigctl to v0.175.0
    https://github.com/alexfalkowski/konfigctl/pull/307

--- area/apps/lean/standort.go
+++ area/apps/lean/standort.go
@@ -11,7 +11,7 @@ func createStandort(ctx *pulumi.Context) error {
 		Name:          "standort",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.174.0",
+		InitVersion:   "0.175.0",
 		Version:       "2.324.0",
 		ConfigVersion: "1.13.0",
 		Resources: &app.Resources{
--- area/apps/lean/web.go
+++ area/apps/lean/web.go
@@ -11,7 +11,7 @@ func createWeb(ctx *pulumi.Context) error {
 		Name:          "web",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.174.0",
+		InitVersion:   "0.175.0",
 		Version:       "0.201.0",
 		ConfigVersion: "1.3.0",
 		Resources: &app.Resources{
