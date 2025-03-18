93b52af295f30be3dad564573442f575bd3e0465
Alex Falkowski <alexrfalkowski@gmail.com>
2025-03-18 19:36:15 +0100

    feat(apps): update versions
    https://github.com/alexfalkowski/go-service/pull/1430

--- area/apps/bezeichner.go
+++ area/apps/bezeichner.go
@@ -8,8 +8,8 @@ func init() {
 		Name:          "bezeichner",
 		Namespace:     "lean",
 		Domain:        "lean-thoughts.com",
-		InitVersion:   "0.213.0",
-		Version:       "1.369.0",
+		InitVersion:   "0.214.0",
+		Version:       "1.370.0",
 		ConfigVersion: "1.15.0",
 		Resources: &app.Resources{
 			CPU:     &app.Range{Min: "125m", Max: "250m"},
