8f652125474f988a634d1d77ebe34460ea1c7350
Alex Falkowski <alexrfalkowski@gmail.com>
2025-04-25 07:12:20 +0200

    feat(gh): add lean-thoughts-ci collaborator
    This will be used to detach my account from CI.

--- internal/gh/gh.go
+++ internal/gh/gh.go
@@ -6,7 +6,6 @@ import (
 
 	v2 "github.com/alexfalkowski/infraops/api/infraops/v2"
 	"github.com/alexfalkowski/infraops/internal/config"
-	"github.com/pulumi/pulumi-github/sdk/v5/go/github"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -120,102 +119,9 @@ func CreateRepository(ctx *pulumi.Context, repo *Repository) error {
 		return fmt.Errorf("%v: %w", repo.Name, err)
 	}
 
+	if err := collaborator(ctx, repo); err != nil {
+		return fmt.Errorf("%v: %w", repo.Name, err)
+	}
+
 	return nil
-}
-
-func repository(ctx *pulumi.Context, repo *Repository) (*github.Repository, error) {
-	t, err := template(repo)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := repo.Checks.Valid(); err != nil {
-		return nil, err
-	}
-
-	args := &github.RepositoryArgs{
-		AllowMergeCommit:    pulumi.Bool(false),
-		AllowRebaseMerge:    pulumi.Bool(false),
-		AllowUpdateBranch:   pulumi.Bool(true),
-		AutoInit:            pulumi.Bool(true),
-		DeleteBranchOnMerge: pulumi.Bool(true),
-		Description:         pulumi.String(repo.Description),
-		HasDownloads:        pulumi.Bool(true),
-		HasIssues:           pulumi.Bool(true),
-		HasProjects:         pulumi.Bool(true),
-		HasWiki:             pulumi.Bool(true),
-		HomepageUrl:         pulumi.String(repo.HomepageURL),
-		IsTemplate:          pulumi.Bool(repo.IsTemplate),
-		Name:                pulumi.String(repo.Name),
-		Archived:            pulumi.Bool(repo.Archived),
-		Pages:               pages(repo),
-		SecurityAndAnalysis: &github.RepositorySecurityAndAnalysisArgs{
-			SecretScanning: &github.RepositorySecurityAndAnalysisSecretScanningArgs{
-				Status: pulumi.String("enabled"),
-			},
-			SecretScanningPushProtection: &github.RepositorySecurityAndAnalysisSecretScanningPushProtectionArgs{
-				Status: pulumi.String("enabled"),
-			},
-		},
-		SquashMergeCommitTitle: pulumi.String("PR_TITLE"),
-		Template:               t,
-		Topics:                 pulumi.ToStringArray(repo.Topics),
-		Visibility:             pulumi.String(repo.Visibility),
-		VulnerabilityAlerts:    pulumi.Bool(true),
-	}
-
-	return github.NewRepository(ctx, repo.Name, args)
-}
-
-func branchProtection(ctx *pulumi.Context, id pulumi.StringInput, repo *Repository) error {
-	args := &github.BranchProtectionArgs{
-		EnforceAdmins:         pulumi.Bool(false),
-		Pattern:               pulumi.String(master),
-		RepositoryId:          id,
-		RequiredLinearHistory: pulumi.Bool(true),
-		RequiredPullRequestReviews: github.BranchProtectionRequiredPullRequestReviewArray{
-			&github.BranchProtectionRequiredPullRequestReviewArgs{
-				DismissStaleReviews:          pulumi.Bool(true),
-				RequiredApprovingReviewCount: pulumi.Int(0),
-			},
-		},
-		RequiredStatusChecks: github.BranchProtectionRequiredStatusCheckArray{
-			&github.BranchProtectionRequiredStatusCheckArgs{
-				Contexts: pulumi.ToStringArray(repo.Checks),
-				Strict:   pulumi.Bool(true),
-			},
-		},
-	}
-	_, err := github.NewBranchProtection(ctx, repo.Name, args)
-
-	return err
-}
-
-func template(repo *Repository) (*github.RepositoryTemplateArgs, error) {
-	if repo.Template == nil {
-		return nil, nil
-	}
-
-	if err := repo.Template.Valid(); err != nil {
-		return nil, err
-	}
-
-	args := &github.RepositoryTemplateArgs{
-		Owner:      pulumi.String(repo.Template.Owner),
-		Repository: pulumi.String(repo.Template.Repository),
-	}
-
-	return args, nil
-}
-
-func pages(repo *Repository) *github.RepositoryPagesArgs {
-	if !repo.EnablePages {
-		return nil
-	}
-
-	return &github.RepositoryPagesArgs{
-		Source: &github.RepositoryPagesSourceArgs{
-			Branch: pulumi.String(master),
-		},
-	}
 }
