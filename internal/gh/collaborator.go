a984e4d439287b994d06beb67f2eee591728606e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-04-25 10:02:23 +0200

    fix(gh): remove restrictions (#1017)
    Only organization repositories can have users and team restrictions

--- internal/gh/collaborator.go
+++ internal/gh/collaborator.go
@@ -5,13 +5,11 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-const bot = "lean-thoughts-ci"
-
 func collaborator(ctx *pulumi.Context, repo *Repository) error {
 	args := &github.RepositoryCollaboratorArgs{
 		Permission: pulumi.String("admin"),
 		Repository: pulumi.String("alexfalkowski/" + repo.Name),
-		Username:   pulumi.String(bot),
+		Username:   pulumi.String("lean-thoughts-ci"),
 	}
 
 	_, err := github.NewRepositoryCollaborator(ctx, repo.Name, args)
