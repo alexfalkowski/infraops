824f462ce5905eeb6a0471a2388cb2ae0ef07ffe
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-04-25 09:53:55 +0200

    feat(gh): add bot to the list of restrictions (#1016)

--- internal/gh/collaborator.go
+++ internal/gh/collaborator.go
@@ -5,11 +5,13 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+const bot = "lean-thoughts-ci"
+
 func collaborator(ctx *pulumi.Context, repo *Repository) error {
 	args := &github.RepositoryCollaboratorArgs{
 		Permission: pulumi.String("admin"),
 		Repository: pulumi.String("alexfalkowski/" + repo.Name),
-		Username:   pulumi.String("lean-thoughts-ci"),
+		Username:   pulumi.String(bot),
 	}
 
 	_, err := github.NewRepositoryCollaborator(ctx, repo.Name, args)
